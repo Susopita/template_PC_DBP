d0c908ede9f0c8728760a4aa02d28b240ab4ee0f
Susopita <thesusot@gmail.com>
2025-05-08 16:30:56 -0500

    WAZAAa

---
+++ src/main/java/com/example/template_PC/common/application/GlobalExceptionHandler.java
@@ -0,0 +1,36 @@
+package com.example.template_PC.common.application;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.example.template_PC.common.dto.ResponseStatusDto;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<ResponseStatusDto> handleResponseStatusException(ResponseStatusException e) {
+        ResponseStatusDto response = new ResponseStatusDto();
+        response.setStatus(e.getStatusCode().value());
+        response.setMessage(e.getReason());
+
+        return new ResponseEntity<>(response, e.getStatusCode());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ResponseStatusDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError firstError = e.getFieldErrors().getFirst();
+
+        ResponseStatusDto response = new ResponseStatusDto();
+        response.setStatus(HttpStatus.BAD_REQUEST.value());
+        response.setMessage(firstError.getField() + ": " + firstError.getDefaultMessage());
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+
+}
---
+++ src/main/java/com/example/template_PC/common/dto/ResponseStatusDto.java
@@ -0,0 +1,11 @@
+package com.example.template_PC.common.dto;
+
+import lombok.Data;
+
+@Data
+public class ResponseStatusDto {
+
+    private Integer status;
+    private String message;
+
+}
